2f9e659f11ff6fb42c1e07cabc559ed3242d2f97
Tetsuaki Otsuki <chimney37@hotmail.com>
2015-04-08 16:46:10 +0900

    Update to Sum of Rectangle

--- SumOf2Rectangles/SumOf2Rectangles/Program.cs
+++ SumOf2Rectangles/SumOf2Rectangles/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Resources;
 using System.Runtime.CompilerServices;
@@ -7,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SqlServer.Server;
 
+//Coordinate system: Assume an y-axis going from bottom of screen to upper and x-axis going from left of screen to right
 namespace SumOf2Rectangles
 {
     public class Point
@@ -20,13 +22,62 @@ namespace SumOf2Rectangles
         }
     }
 
+    //orthogonal lines only
+    public class Line
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Line()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("(x={0},y={1})",x,y);
+        }
+    }
+
+    //clockwise direction
+    public class Edge
+    {
+        public Point StartPoint { get; set; }
+        public Point EndPoint { get; set; }
+
+        public int GetLengthSquare()
+        {
+            return Math.Abs(EndPoint.y*EndPoint.y - EndPoint.x*EndPoint.x);
+        }
+
+        public Line GetLine()
+        {
+            //check for zero length edges
+            if (GetLengthSquare() != 0)
+            {
+                //horizontal line
+                if (EndPoint.x != StartPoint.x && EndPoint.y == StartPoint.y)
+                {
+                    return new Line() {x = 0, y = EndPoint.y};
+                }
+
+                //vertical line
+                if (EndPoint.y != StartPoint.y && EndPoint.x == StartPoint.x)
+                {
+                    return new Line() { x = EndPoint.x, y = 0};
+                }
+            }
+            return null;
+        }
+    }
+
     public class Rect
     {
-        //Rectangle class : Assume an y-axis going from bottom of screen to upper and x-axis going from left of screen to right
         public Point Ul { get; set; }
         public Point Lr { get; set; }
 
-        public static bool Overlap(Rect a, Rect b)
+        public static bool Intersects(Rect a, Rect b)
         {
             return (a.Ul.x <= b.Lr.x &&
                 a.Ul.y >= b.Lr.y &&
@@ -34,15 +85,71 @@ namespace SumOf2Rectangles
                 a.Lr.y <= b.Ul.y);
         }
 
-        public static bool Intersect(Rect a, Point b)
+        public static bool Intersects(Rect a, Point b)
         {
-            //boundary condition : equal values => interesect
-            return (a.Ul.x <= b.x && b.x <= a.Lr.x) && (a.Lr.y <= b.y && b.y <= a.Ul.y);
+            //boundary condition : equal values does not mean intersect
+            return XIntersects(a, b) && YIntersects(a, b);
+        }
+
+        private static bool XIntersects(Rect a, Point b)
+        {
+            return (a.Ul.x < b.x && b.x < a.Lr.x);
+        }
+
+        private static bool YIntersects(Rect a, Point b)
+        {
+            return (a.Lr.y < b.y && b.y < a.Ul.y);
+        }
+
+        public static Point Intersects(Rect a, Edge b)
+        {
+            //vertical line
+            if (b.GetLine().x != 0 && b.GetLine().y == 0)
+            {
+                //if intersect start point
+                if (Intersects(a, new Point(){x = b.StartPoint.x, y = b.StartPoint.y}))
+                {
+                    return new Point(){x = b.GetLine().x, y=a.Ul.y};
+                }
+
+                //if intersect end point
+                if (Intersects(a, new Point() { x = b.EndPoint.x, y = b.EndPoint.y }))
+                {
+                    return new Point() { x = b.GetLine().x, y = a.Lr.y };
+                }
+            }
+            //horizontal line
+            else if (b.GetLine().x == 0 && b.GetLine().y != 0)
+            {
+                //if intersect start point
+                if (Intersects(a, new Point() { x = b.StartPoint.x, y = b.StartPoint.y }))
+                {
+                    return new Point() { x = b.GetLine().x, y = a.Ul.y };
+                }
+
+                //if intersect end point
+                if (Intersects(a, new Point() { x = b.EndPoint.x, y = b.EndPoint.y }))
+                {
+                    return new Point() { x = b.GetLine().x, y = a.Lr.y };
+                }
+            }
+            //invalid edge
+            return null;
         }
 
         public int GetArea()
         {
-            return (Lr.x - Ul.x)*(Ul.y - Lr.y);
+            return GetWidth() * GetHeight();
+        }
+
+        public int GetWidth()
+        {
+            return (Lr.x - Ul.x);
+        }
+
+        public int GetHeight()
+        {
+            return (Ul.y - Lr.y);
         }
 
         public List<Point> GetVertices()
@@ -52,6 +159,7 @@ namespace SumOf2Rectangles
             Point ur = new Point() {x = Lr.x, y = Ul.y};
             Point ul = new Point() {x = Ul.x, y = Ul.y};
 
+            //Vertices are defined clockwise
             return new List<Point>()
             {
                 lr,
@@ -61,22 +169,45 @@ namespace SumOf2Rectangles
             };
         }
 
-        public static List<Point> GetIntersections(Rect a, Rect b)
+        public List<Edge> GetEdges()
+        {
+            //edges are defined clockwise: starting from lower (bottom) edge
+            Edge lower = new Edge() { StartPoint = new Point() { x = Lr.x, y = Lr.y }, EndPoint = new Point() { x = Ul.x, y = Lr.y } };
+            Edge left = new Edge() { StartPoint = new Point() { x = Ul.x, y = Lr.y }, EndPoint = new Point(){x = Ul.x, y = Ul.y }};
+            Edge upper = new Edge() { StartPoint = new Point() { x = Ul.x, y = Ul.y }, EndPoint = new Point(){x = Lr.x, y = Ul.y }};
+            Edge right = new Edge() { StartPoint = new Point() { x = Lr.x, y = Ul.y }, EndPoint = new Point() { x = Lr.x, y = Lr.y } };
+
+            return new List<Edge>()
+            {
+                lower,
+                left,
+                upper,
+                right
+            };
+        }
+
+        public static List<Point> GetVertexIntersections(Rect a, Rect b)
         {
             List<Point> pts = new List<Point>();
             foreach (Point p in a.GetVertices())
             {
-                if(Intersect(b,p))
+                if(Intersects(b,p))
                     pts.Add(p);
             }
 
             foreach (Point p in b.GetVertices())
             {
-                if(Intersect(a, p))
+                if(Intersects(a, p))
                     pts.Add(p);
             }
 
             return pts;
+        }
+
+        public static List<Line> GetEdgeIntersections(Rect a, Rect b)
+        {
+            List<Line> edges = new List<Line>();
+            return edges;
         }
     }
 
@@ -97,22 +228,23 @@ namespace SumOf2Rectangles
                 Ul = new Point() {x =0, y= 2}
             };
 
-            Point pt1 = new Point() {x = 2, y = 1};
-
-            Console.WriteLine("Rectangle overlap = {0}", Rect.Overlap(a,b) );
+            Console.WriteLine("Rectangle overlap = {0}", Rect.Intersects(a,b) );
             Console.WriteLine("Rectangle A area = {0}", a.GetArea());
             Console.WriteLine("Rectangle B area = {0}", b.GetArea());
-            Console.WriteLine("Rectangle B intersects Point pt1 = {0}", Rect.Intersect(b, pt1));
 
-            Console.WriteLine("Vertices from A=");
-            foreach(Point p in a.GetVertices())
-                Console.Write(p);
+            Console.Write("Vertices from A=");
+            a.GetVertices().ForEach(Console.Write);
+            Console.WriteLine();
+
+            Console.Write("Edges from A=");
+            a.GetEdges().ForEach(Console.Write);
             Console.WriteLine();
 
             Console.WriteLine("Vertices from A in B or B in A");
-            foreach(Point p in Rect.GetIntersections(a,b))
-                Console.Write(p);
+            Rect.GetVertexIntersections(a,b).ForEach(Console.Write);
             Console.WriteLine();
+
+
 
             Console.WriteLine("Press Enter to exit");
             Console.ReadLine();
---
+++ SumOf2Rectangles/SumRectangleUnitTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using SumOf2Rectangles;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace SumRectangleUnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [TestCase(2, 0, 1, 3, 3, 1, 0, 2)]         //this case happens when no vertices overlap but the edges cross each other
+        [TestCase(3, 1, 0, 2,2, 0, 1, 3 )]  //same as above but rects interchanged
+        [TestCase(4,1,2,3,3,2,1,4)] //for single vertex overlap
+        [TestCase(3,2,1,4,4,1,2,3)] //same as above but rects interchanged
+        [TestCase(4, 2, 1, 4, 3, 1, 2, 3)]  //for 2 vertices overlap
+        [TestCase(3, 1, 2, 3,4, 2, 1, 4)]  //same a above but rects interchanged
+        public void TestRectsOverlap(int aLrX, int aLrY,int aUlX,int aUlY, int bLrX, int bLrY,int bUlX,int bUlY)
+        {
+            Rect a = new Rect()
+            {
+                Lr = new Point() { x = aLrX, y = aLrY },
+                Ul = new Point() { x = aUlX, y = aUlY }
+            };
+
+            Rect b = new Rect()
+            {
+                Lr = new Point() { x = bLrX, y = bLrY },
+                Ul = new Point() { x = bUlX, y = bUlY }
+            };
+
+            Assert.AreEqual(true, Rect.Intersects(a, b));
+
+        }
+    }
+}
