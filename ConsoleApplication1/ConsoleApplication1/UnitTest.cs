8760cb4130a3703b7f5c088d7a2110f64b8bea5a
Tetsuaki Otsuki <chimney37@hotmail.com>
2015-04-08 21:15:50 +0900

    Many codility tests. on time complexity and counting elements.

---
+++ ConsoleApplication1/ConsoleApplication1/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApplication1
+{
+    class UnitTest
+    {
+        [TestCase(1, 1000000000, 1, Result = 999999999)]
+        [TestCase(10, 85, 100, Result = 1)]
+        [TestCase(10,85,30,Result = 3)]
+        public int Test(int X,int Y, int D)
+        {
+            return Solution.solution(X, Y, D);
+        }
+    }
+}
