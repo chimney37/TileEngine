d2f5621704171645703cbe8696e06cbcb8bc6646
Tetsuaki Otsuki <chimney37@hotmail.com>
2015-04-11 22:28:35 +0900

    Example of Greedy algorithm. Maximum overlap segments

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -8,6 +8,7 @@ using System.Globalization;
 using System.Linq.Expressions;
 using System.Xml.Schema;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace ConsoleApplication1
 {
@@ -126,13 +127,94 @@ namespace ConsoleApplication1
             //int[] A = {2, 1, 5, 1, 2, 2, 2};
             //Debug.WriteLine(Solution.solution_MinMaxDivision(3,5,A));
 
-            int[] A = { -5, -3, -1, 0, 3, 6 };
-            Debug.WriteLine(Solution.solution_DistinctAbs3(A));
+            //int[] A = { -5, -3, -1, 0, 3, 6 };
+            //Debug.WriteLine(Solution.solution_DistinctAbs3(A));
+
+            int[] A = {1, 3, 7, 9, 9};
+            int[] B = {5, 6, 8, 9, 10};
+
+            Debug.WriteLine(Solution.solution_MaximumOverlappingSegments(A, B));
         }
     }
 
     internal class Solution
     {
+        /// <summary>
+        /// Located on a line are N segments, numbered from 0 to N − 1, 
+        /// whose positions are given in zero-indexed arrays A and B. 
+        /// For each I (0 ≤ I < N) the position of segment I is from A[I] to B[I] (inclusive). 
+        /// The segments are sorted by their ends, which means that B[K] ≤ B[K + 1] for K such that 0 ≤ K < N − 1.
+        /// 
+        /// Two segments I and J, such that I ≠ J, are overlapping if they share at least one common point. 
+        /// In other words, A[I] ≤ A[J] ≤ B[I] or A[J] ≤ A[I] ≤ B[J].
+        /// 
+        /// A[0] = 1    B[0] = 5
+        ///A[1] = 3    B[1] = 6
+        ///A[2] = 7    B[2] = 8
+        ///A[3] = 9    B[3] = 9
+        ///A[4] = 9    B[4] = 10
+        /// 
+        /// The size of a non-overlapping set containing a maximal number of segments is 3. 
+        /// For example, possible sets are {0, 2, 3}, {0, 2, 4}, {1, 2, 3} or {1, 2, 4}. 
+        /// There is no non-overlapping set with four segments.
+        /// 
+        /// that, given two zero-indexed arrays A and B consisting of N integers, 
+        /// returns the size of a non-overlapping set containing a maximal number of segments.
+        /// 
+        /// For example, given arrays A, B shown above, the function should return 3, as explained above.
+        /// 
+        /// •N is an integer within the range [0..30,000];
+        /// •each element of arrays A, B is an integer within the range [0..1,000,000,000];
+        /// •A[I] ≤ B[I], for each I (0 ≤ I < N);
+        /// •B[K] ≤ B[K + 1], for each K (0 ≤ K < N − 1).
+        /// 
+        /// •expected worst-case time complexity is O(N);
+        /// •expected worst-case space complexity is O(N), beyond input storage
+        ///  (not counting the storage required for input arguments).
+        /// 
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <returns></returns>
+        public static int solution_MaximumOverlappingSegments(int[] A, int[] B)
+        {
+            //consider the greedy approach, it's not going to find all possibilities though
+            //try finding the maximum number of non-overlapping segments in a single loop
+
+            //if empty, return 0
+            if (A.Length == 0)
+                return 0;
+
+            //if there is 1 segment only, special case, already found
+            if (A.Length == 1)
+                return 1;
+
+            //consider the first segment already found (since if you find a segment that is not overlapping with it,
+            //you consider finding 2 segments)
+            int maximalnonoverlaps = 1;
+            int prev_end = B[0];
+
+            //compare each segments end with the next segments start point
+            //if next segments start point is more than the previous segments end point, there is no overlap
+            //in that case, increment
+            for (int cur = 1; cur < A.Length; cur++)
+            {
+                if (A[cur] > prev_end)
+                {
+                    maximalnonoverlaps++;
+                    prev_end = B[cur];
+                }
+            }
+
+            return maximalnonoverlaps;
+        }
+
+        private static bool IsOverlapSegment(int a1, int b1, int a2, int b2)
+        {
+            return (a1 <= a2 && a2 <= b1 || a2 <= a1 && a1 <= b2);
+        }
+
+
         /// <summary>
         /// A non-empty zero-indexed array A consisting of N numbers is given. 
         /// The array is sorted in non-decreasing order. 
